e7b13e00c5301ed0efe2130b25ac7473273cc0d8
Léo Ruer <126884638+LeoRuer@users.noreply.github.com>
2024-09-09 10:43:38 +0200

    Add files via upload

---
+++ ESP32 MicroPython/esp32.wan.py
@@ -0,0 +1,12 @@
+#Script de test en MicroPython pour ESP32
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('ssid', 'key')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ipconfig('addr4'))
